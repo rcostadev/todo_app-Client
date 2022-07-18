ed12f8a084083738fa653aaac7e6b7a21aabe2b3
ricardo costa <elberricardo2001@gmail.com>
2022-07-18 13:54:22 -0100

    Fim Input Todo

---
+++ src/components/inputTodo.js
@@ -0,0 +1,43 @@
+import React, { Fragment, useState} from "react";
+
+
+
+const InputTodo = () => {
+
+    const [descricao, setDescricao] = useState("");
+
+    const onSubmitForm = async (e) => {
+        e.preventDefault();
+        try {
+            const body = {descricao};
+            const response = await fetch("http://localhost:5000/todos" ,{
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(body)
+            });
+
+            console.log(response);
+            
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+    return(
+        <Fragment>
+            <h1 className="text-center mt-5">Todo List</h1>
+            <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+                <input type="text" 
+                className="form-control"
+                value={descricao}
+                onChange={e => setDescricao(e.target.value)}
+                />
+                <button className="btn btn-sucess">Adicionar</button>
+            </form>
+            
+        </Fragment>
+        
+    )
+};
+
+export default InputTodo;
