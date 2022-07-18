bec36e9da4524939f26cf0be2126e221f591aa64
ricardo costa <elberricardo2001@gmail.com>
2022-07-18 18:10:00 -0100

    Editar e Deletar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, {Fragment} from "react"
 import './App.css';
 
 import InputTodo from "./components/inputTodo";
+import ListTodos from "./components/ListTodos";
 
 function App() {
   return (
     <Fragment>
       <div className="container">
         <InputTodo/>
+        <ListTodos/>
       </div>
     </Fragment>  
   );
--- src/components/inputTodo.js
+++ src/components/inputTodo.js
@@ -16,7 +16,7 @@ const InputTodo = () => {
                 body: JSON.stringify(body)
             });
 
-            console.log(response);
+            window.location = "/";
             
         } catch (err) {
             console.error(err.message)
